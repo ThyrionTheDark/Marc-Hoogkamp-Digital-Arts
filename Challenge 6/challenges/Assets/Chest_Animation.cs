5c08915799e8574a0cc110db7ebda6868dc3be4d
ThyrionTheDark <marc.hoogkamp@hotmail.com>
2017-03-10 18:46:16 +0100

    challenges
    all of it

---
+++ Challenge 6/challenges/Assets/Chest_Animation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chest_Animation : MonoBehaviour {
+    public SpriteRenderer lockImage;
+    public GameObject chestLight;
+    private float alpha;
+    public float fade;
+
+    void Awake()
+    {
+
+        print("hi?");
+        alpha = lockImage.color.a;
+    }
+
+    void Update()
+    {
+        if(lockImage.color.a > 0)
+        {
+            alpha -= Time.deltaTime;
+            lockImage.color = new Color(1, 1, 1, alpha);
+        }
+    }
+}
